7c4a7b941cb79852d4c424c307ebe526716f323d
Luís Eduardo Monteiro Lima <monteirodemolay@gmail.com>
2026-07-23 14:03:00 -0300

    Add docs

---
+++ src/constants/documentStatus.js
@@ -0,0 +1,9 @@
+export const DOCUMENT_STATUS = {
+  VIGENTE: "Vigente",
+  REVOGADO: "Revogado",
+  ALTERADO: "Alterado",
+  SUSPENSO: "Suspenso",
+  EM_ANALISE: "Em análise"
+};
+
+export default DOCUMENT_STATUS;
